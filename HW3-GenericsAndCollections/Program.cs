df3f4d92c9111a952dadc5a51e4d45f1cd93d1f4
Alexey Babak <Alexey.Babak@MyIB.com>
2024-03-26 18:52:13 +0100

    added Point class

--- HW3-GenericsAndCollections/Program.cs
+++ HW3-GenericsAndCollections/Program.cs
@@ -1,6 +1,7 @@
 ﻿using HW3_GenericsAndCollections;
 
 // Complex number
+Console.WriteLine("\nComplex number:");
 var complexNumber1 = new ComplexNumber<int>(2, 3);
 var complexNumber2 = new ComplexNumber<int>(1, 4);
 
@@ -12,4 +13,16 @@ sum.Print();
 Console.WriteLine("Diff:");
 difference.Print();
 
-// 
+
+// Point
+Console.WriteLine("\nPoint Class:");
+var myPoint = new Point(5.0, 10.0);
+myPoint.PrintCoordinates();
+
+myPoint.X = 20.0;
+myPoint.Y = 30.0;
+myPoint.PrintCoordinates();
+
+Console.WriteLine("\nNext Task");
+
+//
